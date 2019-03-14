f14e75c9222f49e3e8cf6342825140e1f249c678
Sathish Kumar Ramalingam <ramsatt@gmail.com>
2019-03-14 17:11:38 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 // include database connection
 include 'config/database.php';
  
